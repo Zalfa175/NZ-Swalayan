9ef217f89adace9bd981077ca3b3e27e8e6a0ed0
Nabila Zalfa Anandita <zalfaanandita@gmail.com>
2024-02-16 16:01:46 +0700

    create user, customer, stuff, and category table

--- database/migrations/2024_02_16_084951_create_stuffs_table.php
+++ database/migrations/2024_02_16_084951_create_stuffs_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stuffs', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->interger('level')->default(0);
-            $table->rememberToken();
+            $table->string('name')->nullable();
+            $table->double('price')->default(0);
+            $table->string('unit')->nullable();
+            $table->interger('status')->default(0);
+            $table->string('id_category')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stuffs');
     }
 };
